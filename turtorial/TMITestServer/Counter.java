bd3136651a22bd3106401f8c5889ec752309ffdd
jan-martens0124 <135309102+jan-martens0124@users.noreply.github.com>
2024-10-10 14:22:14 +0200

    nieuwe setup

--- turtorial/TMITestServer/Counter.java
+++
@@ -1,5 +0,0 @@
-import java.rmi.Remote; import java.rmi.RemoteException;
-public interface Counter extends Remote {
-
-    int berekenSom(int a, int b) throws RemoteException; int berekenVerschil(int a, int b) throws
-            RemoteException; }
